6e40ac591fe3649bd534a1433a2a0908e1e1e85a
Roman Alicando <romanaaav@gmail.com>
2023-10-02 22:34:57 -0300

    First commit.

---
+++ src/test/java/com/romanaaav/payloadcompressionexample/PayloadCompressionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanaaav.payloadcompressionexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayloadCompressionExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
